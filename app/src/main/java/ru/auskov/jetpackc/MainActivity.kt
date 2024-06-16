d3b980f27a2783b9066d051725c11ccb7c0ad370
andrewuskov <andreuskov2211@yandex.ru>
2024-06-16 11:39:32 +0300

    topbar menu

--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -1,29 +1,72 @@
 package ru.auskov.jetpackc
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText("World")
+            MainScreen(applicationContext)
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun GreetingText(name: String) {
-    Text(text = "Hello $name!", color = MaterialTheme.colorScheme.error)
-}
+fun MainScreen(context: Context) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "Menu")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "edit"
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "delete"
+                        )
+                    }
+                },
+            )
+        }
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingText("Android")
+    }
 }
