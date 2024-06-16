a2691f42dce1b82c45be4335df0594ac11555794
andrewuskov <andreuskov2211@yandex.ru>
2024-06-16 14:41:08 +0300

    snackbar

--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -15,10 +17,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +45,24 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(context: Context) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = {
+             SnackbarHost(
+                 hostState = snackbarHostState
+             ) {
+                 Snackbar(
+                     snackbarData = it,
+                     containerColor = Color.White,
+                     contentColor = Color.Green,
+                     actionColor = Color.Blue,
+                     shape = RoundedCornerShape(20.dp),
+                     modifier = Modifier.padding(bottom = 30.dp)
+                 )
+             }
+        },
         topBar = {
             TopAppBar(
                 colors = topAppBarColors(
@@ -57,7 +86,18 @@ fun MainScreen(context: Context) {
                             contentDescription = "edit"
                         )
                     }
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            val result = snackbarHostState.showSnackbar(
+                                "Item deleted",
+                                "Undone"
+                            )
+
+                            if (result == SnackbarResult.ActionPerformed) {
+                                Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.Filled.Delete,
                             contentDescription = "delete"
