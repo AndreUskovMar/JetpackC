9862ee33b2f1a098ea11fa62522c1b71559c384d
andrewuskov <andreuskov2211@yandex.ru>
2024-06-23 15:42:09 +0300

    drawer modal

--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
@@ -24,6 +27,7 @@ import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -45,68 +49,81 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(context: Context) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
 
-    Scaffold(
-        snackbarHost = {
-             SnackbarHost(
-                 hostState = snackbarHostState
-             ) {
-                 Snackbar(
-                     snackbarData = it,
-                     containerColor = Color.White,
-                     contentColor = Color.Green,
-                     actionColor = Color.Blue,
-                     shape = RoundedCornerShape(20.dp),
-                     modifier = Modifier.padding(bottom = 30.dp)
-                 )
-             }
-        },
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(text = "Menu")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
-                    }) {
-                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Edit,
-                            contentDescription = "edit"
-                        )
-                    }
-                    IconButton(onClick = {
-                        coroutineScope.launch {
-                            val result = snackbarHostState.showSnackbar(
-                                "Item deleted",
-                                "Undone"
-                            )
-
-                            if (result == SnackbarResult.ActionPerformed) {
-                                Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Delete,
-                            contentDescription = "delete"
-                        )
-                    }
-                },
-            )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerHeader()
+                DrawerBody()
+            }
         }
     ) {
+        Scaffold(
+            snackbarHost = {
+                 SnackbarHost(
+                     hostState = snackbarHostState
+                 ) {
+                     Snackbar(
+                         snackbarData = it,
+                         containerColor = Color.White,
+                         contentColor = Color.Green,
+                         actionColor = Color.Blue,
+                         shape = RoundedCornerShape(20.dp),
+                         modifier = Modifier.padding(bottom = 30.dp)
+                     )
+                 }
+            },
+            topBar = {
+                TopAppBar(
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text(text = "Menu")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Edit,
+                                contentDescription = "edit"
+                            )
+                        }
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                val result = snackbarHostState.showSnackbar(
+                                    "Item deleted",
+                                    "Undone"
+                                )
 
+                                if (result == SnackbarResult.ActionPerformed) {
+                                    Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Delete,
+                                contentDescription = "delete"
+                            )
+                        }
+                    },
+                )
+            }
+        ) {
+
+        }
     }
 }
