974ecbbc50f841fe31d79e9ce1c6fb2eea3f62ac
andrewuskov <andreuskov2211@yandex.ru>
2024-07-09 16:24:46 +0300

    stack navigation

--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -3,27 +3,36 @@ package ru.auskov.jetpackc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.auskov.jetpackc.screens.Screen1
+import ru.auskov.jetpackc.screens.Screen2
+import ru.auskov.jetpackc.screens.Screen3
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText("World")
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "screen_1") {
+                composable("screen_1") {
+                    Screen1 {
+                        navController.navigate("screen_2")
+                    }
+                }
+                composable("screen_2") {
+                    Screen2 {
+                        navController.navigate("screen_3")
+                    }
+                }
+                composable("screen_3") {
+                    Screen3 {
+                        navController.popBackStack("screen_1", false)
+                    }
+                }
+            }
         }
     }
-}
-
-@Composable
-fun GreetingText(name: String) {
-    Text(text = "Hello $name!", color = MaterialTheme.colorScheme.error)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingText("Android")
 }
