e76a69c9a6ad0c39c142ebc19ade5f1a1eab313e
andrewuskov <andreuskov2211@yandex.ru>
2024-07-27 16:37:27 +0300

    graphics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.auskov.jetpackc"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation ("co.yml:ycharts:2.1.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -3,27 +3,112 @@ package ru.auskov.jetpackc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import co.yml.charts.axis.AxisData
+import co.yml.charts.common.model.Point
+import co.yml.charts.ui.linechart.LineChart
+import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.IntersectionPoint
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
+import co.yml.charts.ui.linechart.model.LineStyle
+import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
+import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
+import co.yml.charts.ui.linechart.model.ShadowUnderLine
+import ru.auskov.jetpackc.ui.theme.JetpackCTheme
+import kotlin.random.Random
+
+const val steps = 10
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText("World")
+            val pointsList = getPointsList()
+            val xAxisData = AxisData.Builder()
+                .axisStepSize(100.dp)
+                .backgroundColor(Color.Transparent)
+                .steps(pointsList.size - 1)
+                .labelData { i -> i.toString() + "d" }
+                .labelAndAxisLinePadding(15.dp)
+                .build()
+
+            val yAxisData = AxisData.Builder()
+                .steps(steps)
+                .backgroundColor(Color.Transparent)
+                .labelAndAxisLinePadding(20.dp)
+                .labelData { i ->
+                    val min = getMinValue(pointsList)
+                    val max = getMaxValue(pointsList)
+                    val yScale = (max - min) / steps.toFloat()
+                    String.format("%.1f", ((i * yScale) + min))
+                }.build()
+
+            JetpackCTheme {
+                val lineChartData = LineChartData(
+                    linePlotData = LinePlotData(
+                        lines = listOf(
+                            Line(
+                                dataPoints = pointsList,
+                                LineStyle(color = Color.Green),
+                                IntersectionPoint(color = Color.Blue, radius = 3.dp),
+                                SelectionHighlightPoint(),
+                                ShadowUnderLine(),
+                                SelectionHighlightPopUp()
+                            )
+                        ),
+                    ),
+                    xAxisData = xAxisData,
+                    yAxisData = yAxisData,
+                    gridLines = GridLines(),
+                    backgroundColor = Color.White
+                )
+
+                LineChart(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    lineChartData = lineChartData
+                )
+            }
         }
     }
 }
 
-@Composable
-fun GreetingText(name: String) {
-    Text(text = "Hello $name!", color = MaterialTheme.colorScheme.error)
+fun getMinValue(list: List<Point>): Float {
+    var min = 100F
+    list.forEach{
+        if (min > it.y) min = it.y
+    }
+
+    return min
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingText("Android")
+fun getMaxValue(list: List<Point>): Float {
+    var max = 0F
+    list.forEach{
+        if (max < it.y) max = it.y
+    }
+
+    return max
+}
+
+fun getPointsList(): List<Point> {
+    val list = ArrayList<Point>()
+
+    for (i in 0..31) {
+        list.add(
+            Point(
+                i.toFloat(),
+                Random.nextInt(50, 100).toFloat()
+            )
+        )
+    }
+
+    return list
 }
