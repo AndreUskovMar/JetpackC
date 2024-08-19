bcc46966a32d87dc9bafd409cb2cdc09c41dcafe
andrewuskov <andreuskov2211@yandex.ru>
2024-08-19 11:57:17 +0300

    qr code scanner

--- app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
+++ app/src/main/java/ru/auskov/jetpackc/MainActivity.kt
@@ -1,29 +1,61 @@
 package ru.auskov.jetpackc
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+import ru.auskov.jetpackc.ui.theme.JetpackCTheme
+
 
 class MainActivity : ComponentActivity() {
+    // Register the launcher and result handler
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText("World")
+            JetpackCTheme {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Button(onClick = {
+                        scan()
+                    }) {
+                        Text(text = "Scan QR")
+                    }
+                }
+            }
         }
     }
-}
 
-@Composable
-fun GreetingText(name: String) {
-    Text(text = "Hello $name!", color = MaterialTheme.colorScheme.error)
-}
+    private fun scan() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a barcode")
+        options.setCameraId(0) // Use a specific camera of the device
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingText("Android")
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+
+        barcodeLauncher.launch(options);
+    }
 }
